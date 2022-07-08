52ce93dc0bcacc5c582c3793197363e7f975529b
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-07 18:21:47 -0700

    feat: init node-fetch

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fetch from 'node-fetch';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "duck-duck-boom" is now active!');
+
+
+	await createAPICall();
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -24,3 +28,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+async function createAPICall() {
+	const response = await fetch('https://api.github.com/users/github');
+	const data = await response.json();
+	
+	console.log(data);
+}
