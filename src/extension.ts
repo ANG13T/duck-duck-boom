c19c34a3c3be7dfcbf0ec8a14528cb85c0740fd6
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-07 23:33:40 -0700

    feat: init disposable2

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,27 @@ import * as vscode from 'vscode';
 import fetch from 'node-fetch';
 
 export function activate(context: vscode.ExtensionContext) {
-	
-	console.log('Congratulations, your extension "duck-duck-boom" is now active!');
 
 	let disposable = vscode.commands.registerCommand('duck-duck-boom.helloWorld', async() => {
 		vscode.window.showInformationMessage('Hello World from Duck Duck Boom!');
 		let results = await createAPICall();
+	});
+
+
+	let disposable2 = vscode.commands.registerCommand('duck-duck-boom.quickInput', async() => {
+		const options: { [key: string]: (context: vscode.ExtensionContext) => Promise<void> } = {
+			showInputBox
+		};
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = Object.keys(options).map(label => ({ label }));
+		quickPick.onDidChangeSelection(selection => {
+			if (selection[0]) {
+				options[selection[0].label](context)
+					.catch(console.error);
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
 	});
 
 	context.subscriptions.push(disposable);
