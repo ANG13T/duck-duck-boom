117437d24bdbf9ead90f1e740c96b347e266faf7
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-07 19:53:50 -0700

    feat: get contents from rubber ducky repo

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import fetch from 'node-fetch';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "duck-duck-boom" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('duck-duck-boom.helloWorld', async() => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Duck Duck Boom!');
 		let results = await createAPICall();
 	});
@@ -24,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
 
 
 async function createAPICall(): Promise<void> {
-	const response = await fetch('https://api.github.com/users/github');
+	// https://api.github.com/users/github
+	const response = await fetch('https://api.github.com/repos/hak5/usbrubberducky-payloads/contents/payloads/library');
 	const data = await response.json();
 
 	console.log(data);
