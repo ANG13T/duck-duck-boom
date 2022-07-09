72a097c15ccf9fc7633fe2b5856fd3842d156d87
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-09 10:12:07 -0700

    feat: write showQuickPick

--- src/controllers/quickPickController.ts
+++ src/controllers/quickPickController.ts
@@ -8,27 +8,27 @@ export class QuickPickController {
     }
 
     public async showQuickPick() {
-        await window.showQuickPick(['Credentials', 'Execution', 'Exfiltration', 'General', 'Incident Response', 'Mobile', 'Phishing', 'Prank', 'Reconnaissance', 'Remote Access'], {
-            placeHolder: 'Choose Payload Category',
-            onDidSelectItem: item => this.showPayloadForCategory(item)
-        });
+        const categories: QuickPickItem[] = ['Credentials', 'Execution', 'Exfiltration', 'General', 'Incident Response', 'Mobile', 'Phishing', 'Prank', 'Reconnaissance', 'Remote Access']
+		.map(label => ({ label }));
+
+        const quickPick = window.createQuickPick();
+        quickPick.items = categories;
+
+        quickPick.onDidTriggerButton(selection => {
+           console.log("hello", selection)
+        })
+
+        quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
+
+        // await window.showQuickPick(categories, {
+        //     placeHolder: 'Choose Payload Category',
+        //     onDidChangeSelection: item => this.showPayloadForCategory(item)
+        // });        
     }
 
     private async showPayloadForCategory(category: string | QuickPickItem) {
         window.showInformationMessage(`Chose ${category} category!`)
-    }
-
-    public async showInputBox() {
-        const result = await window.showInputBox({
-            value: 'abcdef',
-            valueSelection: [2, 4],
-            placeHolder: 'For example: fedcba. But not: 123',
-            validateInput: text => {
-                window.showInformationMessage(`Validating: ${text}`);
-                return text === '123' ? 'Not 123!' : null;
-            }
-        });
-        window.showInformationMessage(`Got: ${result}`);
     }
 
     public async createQuickPick(context: ExtensionContext) {
