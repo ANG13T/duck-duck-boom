82aa521d2cd8f329ed4b248afe8399c7d53daf85
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-10 15:52:51 -0700

    feat: get most categories for payload controller

--- src/controllers/payloadController.ts
+++ src/controllers/payloadController.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { PayloadResponse } from '../utils/payloadTypes';
 
 export class PayloadController {
-    private payloads = {
+    private payloads: any = {
         'credentials': [],
         'execution': [], 
         'exfiltration': [],
@@ -29,14 +29,18 @@ export class PayloadController {
         }
     }
 
-    public async getPayloadsForCategories() {
-        const files = ['credentials', 'execution', 'exfiltration', 'general', 'incident_response', 'mobile', 'phishing', 'prank', 'recon', 'remote_access'];
-        files.forEach(async(file) => {
-            let results = await this.runPayloadsByCategory(file);
-            this.payloads[file] = results;
+    public async getPayloadsForCategories() : Promise<any>{
+        return new Promise(async(resolve) => {
+            const files = ['credentials', 'execution', 'exfiltration', 'general', 'incident_response', 'mobile', 'phishing', 'prank', 'recon', 'remote_access'];
+            await files.forEach(async(file, index) => {
+                let results = await this.runPayloadsByCategory(file);
+                this.payloads[file] = results;
+                if(file == 'remote_access') {
+                    resolve(this.payloads);
+                }
+            });
         })
     }
-
 
     public async runPayloadsByCategory(selectedFile: string) {
         const response = await fetch(`https://api.github.com/repos/hak5/usbrubberducky-payloads/contents/payloads/library/${selectedFile}`);
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,21 @@
 import {commands, ExtensionContext, window, workspace} from 'vscode';
 import { CopyLinkController } from './controllers/copyLinkController';
+import { PayloadController } from './controllers/payloadController';
 import { QuickPickController } from './controllers/quickPickController';
 import { ViewsController } from './controllers/viewsController';
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
 	let quickPickController = new QuickPickController(context);
 	let viewsController = new ViewsController();
 	let copyLinkController = new CopyLinkController();
+	let payloadController = new PayloadController();
+
+	console.log("runnung")
+	await payloadController.getPayloadsForCategories().then((result) => {
+		if(result) {
+			console.log("WYAYAY", result)
+		}
+	})
 
 	// get all data for payloads (catgeory -> payload name) in beginning and pass data down to controllers
 
