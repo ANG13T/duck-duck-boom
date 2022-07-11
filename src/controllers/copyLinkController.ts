26f29eaf3a7708c8f7b525355371da72e93e6cce
angelina-tsuboi <angelina.t1832@gmail.com>
2022-07-10 17:17:18 -0700

    feat: init TreeDataProvider

--- src/controllers/copyLinkController.ts
+++ src/controllers/copyLinkController.ts
@@ -1,4 +1,5 @@
 const path = require('path');
+import * as vscode from 'vscode';
 
 export class CopyLinkController {
     constructor() {
@@ -9,3 +10,41 @@ export class CopyLinkController {
         const parentDir = path.basename(path.dirname(__filename));
     }
 }
+
+export class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+    onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
+  
+    data: TreeItem[];
+  
+    constructor() {
+      this.data = [new TreeItem('cars', [
+        new TreeItem(
+            'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
+        new TreeItem(
+            'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
+      ])];
+    }
+  
+    getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+      return element;
+    }
+  
+    getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+      if (element === undefined) {
+        return this.data;
+      }
+      return element.children;
+    }
+  }
+  
+  class TreeItem extends vscode.TreeItem {
+    children: TreeItem[]|undefined;
+  
+    constructor(label: string, children?: TreeItem[]) {
+      super(
+          label,
+          children === undefined ? vscode.TreeItemCollapsibleState.None :
+                                   vscode.TreeItemCollapsibleState.Expanded);
+      this.children = children;
+    }
+  }
